ef995ebe787a28e75dc1faea1581dbab041793e1
Williams Gutierrez Giraldo - Ceiba Software <williams.gutierrez@ceiba.com.co>
2022-02-16 17:25:22 -0500

    Se agregan componentes de angular material para listar viviendas

--- src/app/feature/material/material.module.ts
+++ src/app/feature/material/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -12,6 +14,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
