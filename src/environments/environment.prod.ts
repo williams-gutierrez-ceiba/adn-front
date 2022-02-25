be440040f67e228674ec5172f64564d18ffb0f86
Williams Gutierrez Giraldo - Ceiba Software <williams.gutierrez@ceiba.com.co>
2022-02-25 11:51:43 -0500

    Se agregan endpoints a archivo environment de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,27 @@
+// export const environment = {
+//   production: true,
+//   endpoint: '/prospectos-vida'
+// };
+
 export const environment = {
-  production: true,
-  endpoint: '/prospectos-vida'
+  productos: {
+    production: true,
+    endpoint: '/prospectos-vida'
+  },
+  usuarios: {
+    production: true,
+    endpoint: 'http://localhost:8081/usuarios'
+  },
+  archivos: {
+    production: true,
+    endpoint: 'http://localhost:8082/archivos'
+  },
+  viviendas: {
+    production: true,
+    endpoint: 'http://localhost:8083/viviendas'
+  },
+  reservas: {
+    production: true,
+    endpoint: 'http://localhost:8084/reservas'
+  }
 };
